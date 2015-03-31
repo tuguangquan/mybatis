f7675c09e1587f4ed24fd5a9c5a7a1da21fd0456
rollenholt <renwenchao888@gmail.com>
2015-03-31 16:02:28 +0800

    增加了一些注释

--- src/main/java/org/apache/ibatis/session/SqlSession.java
+++ src/main/java/org/apache/ibatis/session/SqlSession.java
@@ -31,23 +31,24 @@ import org.apache.ibatis.executor.BatchResult;
 /**
  * 这是MyBatis主要的一个类，用来执行SQL，获取映射器，管理事务
  *
+ * 通常情况下，我们在应用程序中使用的Mybatis的API就是这个接口定义的方法。
+ *
  */
 public interface SqlSession extends Closeable {
 
-  //语句执行方法
-  //这些方法被用来执行SELECT，INSERT，UPDATE和DELETE语句。
   /**
    * Retrieve a single row mapped from the statement key
-   * 获取一条记录
-   * @param <T> the returned object type
-   * @param statement
-   * @return Mapped object
+   * 根据指定的SqlID获取一条记录的封装对象
+   * @param <T> the returned object type 封装之后的对象类型
+   * @param statement sqlID
+   * @return Mapped object 封装之后的对象
    */
   <T> T selectOne(String statement);
 
   /**
    * Retrieve a single row mapped from the statement key and parameter.
-   * 获取一条记录
+   * 根据指定的SqlID获取一条记录的封装对象，只不过这个方法容许我们可以给sql传递一些参数
+   * 一般在实际使用中，这个参数传递的是pojo，或者Map或者ImmutableMap
    * @param <T> the returned object type
    * @param statement Unique identifier matching the statement to use.
    * @param parameter A parameter object to pass to the statement.
@@ -57,7 +58,7 @@ public interface SqlSession extends Closeable {
 
   /**
    * Retrieve a list of mapped objects from the statement key and parameter.
-   * 获取多条记录
+   * 根据指定的sqlId获取多条记录
    * @param <E> the returned list element type
    * @param statement Unique identifier matching the statement to use.
    * @return List of mapped object
@@ -66,7 +67,7 @@ public interface SqlSession extends Closeable {
 
   /**
    * Retrieve a list of mapped objects from the statement key and parameter.
-   * 获取多条记录
+   * 获取多条记录，这个方法容许我们可以传递一些参数
    * @param <E> the returned list element type
    * @param statement Unique identifier matching the statement to use.
    * @param parameter A parameter object to pass to the statement.
@@ -77,7 +78,14 @@ public interface SqlSession extends Closeable {
   /**
    * Retrieve a list of mapped objects from the statement key and parameter,
    * within the specified row bounds.
-   * 获取多条记录,加上分页
+   * 获取多条记录，这个方法容许我们可以传递一些参数，不过这个方法容许我们进行
+   * 分页查询。
+   *
+   * 需要注意的是默认情况下，Mybatis为了扩展性，仅仅支持内存分页。也就是会先把
+   * 所有的数据查询出来以后，然后在内存中进行分页。因此在实际的情况中，需要注意
+   * 这一点。
+   *
+   * 一般情况下公司都会编写自己的Mybatis 物理分页插件
    * @param <E> the returned list element type
    * @param statement Unique identifier matching the statement to use.
    * @param parameter A parameter object to pass to the statement.
